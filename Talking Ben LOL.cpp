a1638e35088e3386eb77d99874ec332529710fed
poopoo <6plosive@protonmail.com>
2022-03-08 19:14:51 -0800

    my first commit

---
+++ Talking Ben LOL.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Ben? (type to talk with Ben):" << endl;
+    string x;
+    string wordList[4] = { "Yes", "No", "Ho ho ho", "hlreurh" };
+    for (int i = 1; i < 100; i++)
+    {
+        string word = wordList[rand() % 4];
+        getline(cin, x);
+        cout<<word<<endl;
+    }
+}
